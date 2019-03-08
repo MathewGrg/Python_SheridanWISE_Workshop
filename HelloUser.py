3f9bb210b89195bfcf2c87d66ff2d41d267cbd66
Mathew George <mathewgrg@users.noreply.github.com>
2019-03-08 00:06:33 -0500

    Initial commit

---
+++ HelloUser.py
@@ -0,0 +1,5 @@
+
+
+userName= input(f'Hi, I am {__name__}. What is your name?')
+
+print(f'Hello {userName}. Nice to meet you.')
---
+++ RandomNum.py
@@ -0,0 +1,9 @@
+import random
+
+#create a random integer between 0 and 10
+randNum = random.randint(0, 10)
+
+#print the random number
+print(f' A random num between 0 and 10 is {randNum}')
+print(f'')
+
